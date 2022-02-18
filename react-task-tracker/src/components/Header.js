c132324fba8ef9e428d14b541a8a9b8307daea24
Kenywil Tiu <tiukenywil11@gmail.com>
2022-02-19 00:40:54 +0800

    Appended the following files to add styling: Header.js, index.css

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,9 +1,13 @@
+import PropTypes from 'prop-types'
+
 // {title} parameter is a destructures variable from "props" variable. 
 // Calling it inside the function would only be {title}, instead of {props.title}
 const Header = ({title}) => {
     return (
         <header>
-            <h1> {title} </h1>
+            <h1 style={{color: 'red', backgroundColor: 'black'}}> 
+                {title} 
+            </h1>
         </header>
     )
 }
@@ -12,5 +16,18 @@ const Header = ({title}) => {
 Header.defaultProps = {
     title: 'Task Tracker Default',
 }
+
+// Initialize property type for 'Header' component, will check if property type matches
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+/*
+--- Initialize stylesheet using javascript function
+const headingStyle = {
+    color: 'red', 
+    backgroundColor: 'black',
+}
+*/
 
 export default Header
