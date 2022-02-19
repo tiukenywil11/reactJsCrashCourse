a718020d45f924dfb83a057ee13dc9e0f87ccee9
Kenywil Tiu <tiukenywil11@gmail.com>
2022-02-19 16:18:42 +0800

    Added the following file: Tasks.js

---
+++ react-task-tracker/src/components/Tasks.js
@@ -0,0 +1,30 @@
+// Example tasks, for testing usage. (This should be part of states)
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 4th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+]
+
+export const Tasks = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
