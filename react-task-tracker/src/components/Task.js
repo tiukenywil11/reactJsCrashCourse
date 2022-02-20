97998cd6721a181a34af485a81ba5d1a85fdcfc3
Kenywil Tiu <tiukenywil11@gmail.com>
2022-02-20 11:45:05 +0800

    Appended the following file to add react-icons: Task.js, package.json, package-lock.json

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -1,8 +1,14 @@
+// Importing Font Awesome icons
+import { FaTimes } from 'react-icons/fa'
+
 // This comppnent would be used on Tasks, to modularize the design per list item
 export const Task = ({ task }) => {
   return (
     <div className='task'>
-        <h3>{task.text}</h3>
+        <h3>
+            {task.text}
+            <FaTimes style={{ color: 'red', cursor: 'pointer' }}/>
+        </h3>
         <p>{task.day}</p>
     </div>
   )
