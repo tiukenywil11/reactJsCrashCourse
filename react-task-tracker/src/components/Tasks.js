bafaffd84346287513d840e7cc6ae47f78944068
Kenywil Tiu <tiukenywil11@gmail.com>
2022-02-20 11:01:18 +0800

    Appended the following file to use states for handling arrays: Tasks.js

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,4 +1,8 @@
-// Example tasks, for testing usage. (This should be part of states)
+// Importing a hook called useState to be able to use states
+import { useState } from 'react'
+
+/*
+--  Example tasks, for testing usage. (This should be part of states)
 const tasks = [
     {
         id: 1,
@@ -19,10 +23,34 @@ const tasks = [
         reminder: false,
     }
 ]
+*/
 
-// Using map function to list out the details of the list
 export const Tasks = () => {
+    // Adding states to the array, 'setTasks' is a function to update the states
+    // States are immutable, and cannot be directly changed (e.g. cannot use tasks.push())
+    // To be able to change the state, call setTasks([...tasks], {})
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 4th at 2:30pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Meeting at School',
+            day: 'Feb 6th at 1:30pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Food Shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: false,
+        }
+    ])
 
+    // Using map function to list out the details of the list
     return (
         <div>
             {tasks.map((task) => (
